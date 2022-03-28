380e0e138b5a61f570470c75718bab693af07cc9
Caio Santos <caiosantos21@gmail.com>
2022-03-28 18:07:28 -0300

    Update item.tsx

--- components/item.tsx
+++ components/item.tsx
@@ -1,17 +1,37 @@
-import React, { FC } from 'react'
-import { StyleSheet, View, Text, Image } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, Text, Image, Alert } from 'react-native';
 import { Obj } from '../src/Interfaces/interface';
 
 import general_styles from '../src/styles/general_styles';
 
 export default function item(props:Obj){
+
+  const [img, setImg] = useState<string>(props.item.img);
+
+  useEffect( () =>{
+    checkYoutubeImg()
+  },[]);
+
+  const checkYoutubeImg = ()=>{
+    const youtube = youtube_parser(img);
+
+    youtube && setImg(`https://img.youtube.com/vi/${youtube}/0.jpg`)
+  }
+
+  
+  function youtube_parser(url:string){
+    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    const match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+  }
+
   return (
     <View style={[general_styles.box, general_styles.flex1]} >
       <View style={[general_styles.center]}>
         <Text style={general_styles.fSize20}>ID: {props.item.id}</Text>
         <Text style={general_styles.fSize20}>Name: {props.item.name}</Text>
       </View>
-      <Image style={[styles.Logo, general_styles.flex1]} source={{uri:props.item.img}}/>
+      <Image style={[styles.Logo, general_styles.flex1]} source={{uri:img}}/>
     </View >
   )
 }
